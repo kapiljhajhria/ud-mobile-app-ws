88ffbc0d9759b493b06e13e9050cddb312cb527d
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-18 21:53:46 +0530

    init

---
+++ src/main/java/com/karg/userrsmanagement/UserrsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.karg.userrsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserrsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserrsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karg/userrsmanagement/UserrsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karg.userrsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserrsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
