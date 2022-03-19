fa0ef02bdc26c6eac577face5eddc1eca8f9c1b3
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-19 22:10:30 +0530

    AuthorizationFilter added

---
+++ src/main/java/com/karg/userrsmanagement/filter/AuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.karg.userrsmanagement.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.karg.userrsmanagement.config.SecurityConstants;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    public AuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(SecurityConstants.HEADER_STRING);
+
+        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+        if (token != null) {
+            token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+
+            Algorithm algorithm = Algorithm.HMAC512(SecurityConstants.TOKEN_SECRET);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .build();
+            DecodedJWT jwt = verifier.verify(token);
+            String decodedUsername = jwt.getSubject();
+
+            if (decodedUsername != null) {
+                return new UsernamePasswordAuthenticationToken(decodedUsername, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+}
