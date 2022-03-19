79d8bf8d5d9b69f61c85549fcaab78d13c645af9
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-19 14:24:06 +0530

    attemptAuthentication Override method implemented

--- src/main/java/com/karg/userrsmanagement/filter/AuthenticationFilter.java
+++ src/main/java/com/karg/userrsmanagement/filter/AuthenticationFilter.java
@@ -1,12 +1,17 @@
 package com.karg.userrsmanagement.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.karg.userrsmanagement.ui.model.request.UserLoginRequestModel;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -18,6 +23,18 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        try {
+            UserLoginRequestModel creds = new ObjectMapper().readValue(request.getInputStream(), UserLoginRequestModel.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getUsername(),
+                            creds.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
