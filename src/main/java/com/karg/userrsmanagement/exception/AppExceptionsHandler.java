9220b584f8a32c0422aaaf077e95f94db9d0b9bf
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-20 01:56:53 +0530

    handling all other types of exception

--- src/main/java/com/karg/userrsmanagement/exception/AppExceptionsHandler.java
+++ src/main/java/com/karg/userrsmanagement/exception/AppExceptionsHandler.java
@@ -27,5 +27,11 @@ public class AppExceptionsHandler {
 //        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 //        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 //    }
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleOtherException(Exception ex, WebRequest request) {
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
