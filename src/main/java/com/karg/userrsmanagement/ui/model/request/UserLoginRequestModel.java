002cc7134c387b7f7c64a65f29a27fb57b30a388
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-19 14:02:32 +0530

    UserLoginRequestModel created

---
+++ src/main/java/com/karg/userrsmanagement/ui/model/request/UserLoginRequestModel.java
@@ -0,0 +1,17 @@
+package com.karg.userrsmanagement.ui.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRequestModel {
+    private String username;
+    private String password;
+
+
+}
