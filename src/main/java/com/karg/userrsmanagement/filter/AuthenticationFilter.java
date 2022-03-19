a9cc55e5f80a5f3db469171107cc4f125f65858d
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-19 14:41:09 +0530

    AuthenticationFilter will return JWT token on successful login

--- src/main/java/com/karg/userrsmanagement/filter/AuthenticationFilter.java
+++ src/main/java/com/karg/userrsmanagement/filter/AuthenticationFilter.java
@@ -1,17 +1,24 @@
 package com.karg.userrsmanagement.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.karg.userrsmanagement.config.SecurityConstants;
 import com.karg.userrsmanagement.ui.model.request.UserLoginRequestModel;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -36,5 +43,21 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        //this method is called when authentication is successful
+        String username = ((User) authResult.getPrincipal()).getUsername();
+
+        // get jwt token
+        Algorithm algorithm = Algorithm.HMAC512(SecurityConstants.TOKEN_SECRET);
+        var token = JWT.create().
+                withSubject(username)
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .sign(algorithm);
+
+        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+
     }
 }
