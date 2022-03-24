06c5b9f33b02623f6d4dc000b36b1a8d109d9866
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-24 19:51:35 +0530

    PasswordResetRequestModel added

---
+++ src/main/java/com/example/photoapp/ui/model/request/PasswordResetRequestModel.java
@@ -0,0 +1,11 @@
+package com.example.photoapp.ui.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordResetRequestModel {
+    private String email;
+
+}
