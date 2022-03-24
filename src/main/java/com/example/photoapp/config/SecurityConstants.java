ea396a6c7428ef7fda84cb387ddd9352d16efa2d
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-24 23:41:16 +0530

    password-reset endpoint made public

--- src/main/java/com/example/photoapp/config/SecurityConstants.java
+++ src/main/java/com/example/photoapp/config/SecurityConstants.java
@@ -15,6 +15,7 @@ public class SecurityConstants {
     public static final String LOGIN_URL = "/api/v1/login";
     public static final String EMAIL_VERIFICATION_URL = "/api/v1/users/email-verification";
     public static final String PASSWORD_RESET_REQUEST_URL = "/api/v1/users/password-reset-request";
+    public static final String PASSWORD_RESET_URL = "/api/v1/users/password-reset";
 
     public static String getTokenSecret() {
         AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
