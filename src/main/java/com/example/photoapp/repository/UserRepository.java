f1aaa4cf4451c611fcc657d62da6b2689eb2e0cd
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-26 14:31:32 +0530

    native sql query using named parameters

--- src/main/java/com/example/photoapp/repository/UserRepository.java
+++ src/main/java/com/example/photoapp/repository/UserRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
@@ -22,4 +25,10 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
             countQuery = "SELECT count(*) from users u where u.EMAIL_VERIFICATION_STATUS = TRUE",
             nativeQuery = true)
     Page<UserEntity> findALlUsersWithConfirmedEmailAddress(Pageable pageableRequest);
+
+    @Query(value = "select * from users u where u.FIRST_NAME = ?1", nativeQuery = true)
+    List<UserEntity> findAllUsersByFirstName(String firstName);
+
+    @Query(value = "select * from users u where u.LAST_NAME = :lName", nativeQuery = true)
+    List<UserEntity> findAllUsersByLastName(@Param("lName") String lastName);
 }
