1b7a812f73076d7d7fc78f1cceec7ca8061347aa
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-26 18:20:21 +0530

    cors related changes in WebSecurity
    - define CorsConfigurationSource
     -

--- src/main/java/com/example/photoapp/config/WebSecurity.java
+++ src/main/java/com/example/photoapp/config/WebSecurity.java
@@ -3,6 +3,7 @@ package com.example.photoapp.config;
 import com.example.photoapp.filter.AuthenticationFilter;
 import com.example.photoapp.filter.AuthorizationFilter;
 import com.example.photoapp.service.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -24,7 +30,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.
+                cors().and().
+                csrf().disable();
 
         http.authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, SecurityConstants.EMAIL_VERIFICATION_URL).permitAll();
@@ -48,6 +56,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
         filter.setFilterProcessesUrl(SecurityConstants.LOGIN_URL);
         return filter;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration corsConfigurationSource = new CorsConfiguration();
+        corsConfigurationSource.setAllowedOrigins(Arrays.asList("*"));
+        corsConfigurationSource.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfigurationSource.setAllowCredentials(true);//for cookies, authorizationHeader, ssl client certificate etc
+        corsConfigurationSource.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", corsConfigurationSource);
+
+        return source;
     }
 
 }
