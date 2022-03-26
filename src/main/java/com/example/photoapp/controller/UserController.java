f167fda39245b9b81c9408cb6debfdb1736ad0c9
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-26 17:50:10 +0530

    allow single origin for a particular method of a controller using annotation @CrossOrigin

--- src/main/java/com/example/photoapp/controller/UserController.java
+++ src/main/java/com/example/photoapp/controller/UserController.java
@@ -179,7 +179,7 @@ public class UserController {
                 .body(EntityModel.of(returnValue, Arrays.asList(userLink, userAddressesLink, selfLink)));
     }
 
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/email-verification", produces = {MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<OperationStatusModel> verifyEmailToken(@RequestParam(name = "token") String token) {
