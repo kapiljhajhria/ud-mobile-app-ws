07095bc1e75e7babdae4822665c6fce2187df3ca
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-26 14:31:41 +0530

    testing native sql queries

--- src/test/java/com/example/photoapp/integration/UserRepositoryIntegrationTest.java
+++ src/test/java/com/example/photoapp/integration/UserRepositoryIntegrationTest.java
@@ -26,36 +26,20 @@ class UserRepositoryIntegrationTest {
     @Autowired
     UserRepository userRepository;
 
-    UserEntity userEntity = new UserEntity();
+    UserEntity userEntity01 = new UserEntity();
+    UserEntity userEntity02 = new UserEntity();
+
+    static boolean recordsCreated = false;
 
     @BeforeEach
     void setUp() {
 
-        //setup userEntity
-
-        userEntity.setEmail("john@email.com");
-        userEntity.setEncryptedPassword("password");
-        userEntity.setEmailVerificationStatus(Boolean.TRUE);
-        userEntity.setFirstName("John");
-        userEntity.setLastName("Doe");
-        userEntity.setUserId("1a2b3c4d5e6f7g8h9i0j");
-
-        AddressEntity addressEntity = new AddressEntity();
-        addressEntity.setCity("New York");
-        addressEntity.setCountry("USA");
-        addressEntity.setAddressId("1a2b3c4d5e6f7g8h9i0jsda");
-        addressEntity.setUserDetails(userEntity);
-        addressEntity.setType("shipping");
-        addressEntity.setStreetName("Wall street");
-        addressEntity.setPostalCode("10001");
-
-        List<AddressEntity> addressEntities = new ArrayList<>();
-        addressEntities.add(addressEntity);
-        userEntity.setAddresses(addressEntities);
-
-        userRepository.save(userEntity);
+        if (!recordsCreated) {
+            createRecords();
+        }
 
     }
+
 
     @Test
     void findALlUsersWithConfirmedEmailAddress() {
@@ -71,5 +55,62 @@ class UserRepositoryIntegrationTest {
 
     }
 
+    @Test
+    public void testFindUsersByFirstName() {
+        String firstName = "John";
+
+        List<UserEntity> userEntities = userRepository.findAllUsersByFirstName(firstName);
+
+        assertNotNull(userEntities);
+        assertEquals(2, userEntities.size());
+    }
+
+    @Test
+    public void testFindUsersByLastName() {
+        String lastName = "Doe";
+
+        List<UserEntity> userEntities = userRepository.findAllUsersByLastName(lastName);
+
+        assertNotNull(userEntities);
+        assertEquals(2, userEntities.size());
+    }
+
+    private void createRecords() {
+        //setup userEntity
+
+        userEntity01.setEmail("john1@email.com");
+        userEntity01.setEncryptedPassword("password");
+        userEntity01.setEmailVerificationStatus(Boolean.TRUE);
+        userEntity01.setFirstName("John");
+        userEntity01.setLastName("Doe");
+        userEntity01.setUserId("1a2b3c4d5e6f7g8h9i0j");
+
+        userEntity02.setEmail("john2@email.com");
+        userEntity02.setEncryptedPassword("password");
+        userEntity02.setEmailVerificationStatus(Boolean.FALSE);
+        userEntity02.setFirstName("John");
+        userEntity02.setLastName("Doe");
+        userEntity02.setUserId("1a2b3c4d5dfdfe6f7g8h9i0j");
+
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setCity("New York");
+        addressEntity.setCountry("USA");
+        addressEntity.setAddressId("1a2b3c4d5e6f7g8h9i0jsda");
+        addressEntity.setUserDetails(userEntity01);
+        addressEntity.setType("shipping");
+        addressEntity.setStreetName("Wall street");
+        addressEntity.setPostalCode("10001");
+
+        List<AddressEntity> addressEntities = new ArrayList<>();
+        addressEntities.add(addressEntity);
+        userEntity01.setAddresses(addressEntities);
+//        userEntity02.setAddresses(addressEntities);
+
+        userRepository.save(userEntity01);
+        userRepository.save(userEntity02);
+
+        recordsCreated = true;
+    }
 
 }
